23430fc904df1ae9d70261eb261456ec9b8bfc18
KC Tang <tkchaw80@gmail.com>
2025-03-09 19:15:39 +0800

    Added mint with native script

--- src/app/page.js
+++ src/app/page.js
@@ -29,6 +29,10 @@ import {
   ForgeScript
 } from "@meshsdk/core";
 
+import { NativeScript } from "@meshsdk/common"
+
+import mesh from "@meshsdk/core";
+
 import {ulid} from 'ulid';
 
 import {MeshPlutusNFTContract} from "@meshsdk/contract"
@@ -52,6 +56,11 @@ export default function Home() {
   const [oracelNFTPrice, setOracleNFTPrice] = useState()
   const [oracleMintTxHash, setOracleMintTxHash] = useState()
   const [oracleMintParamUtxo, setOracleMintParamUtxo] = useState()
+  // const [mintNFTHeliosLoading, setMintNFTHeliosLoading] = useState(false)
+  // const [mintNFTHeliosTxHash, setMintNFTHeliosTxHash] = useState()
+  const [mintNativeLoading, setMintNativeLoading] = useState(false)
+  const [mintNativeTxHash, setMintNativeTxHash] = useState()
+  const [nativeScriptPolicyId, setNativeScriptPolicyId] = useState()
 
   const [nftMetadata, setNFTMetadata] = useState()
   const [nftMintingPlutus, setNftMintingPlutus] = useState(false)
@@ -77,11 +86,11 @@ export default function Home() {
     }
   }, [isLoggedIn])
 
-  useEffect(() => {
-    if(walletAddress) {
-      memberSignIn(walletAddress)  // after logged in to web3auth, we sign in to our backend to get member profile
-    }
-  }, [walletAddress])
+  // useEffect(() => {
+  //   if(walletAddress) {
+  //     memberSignIn(walletAddress)  // after logged in to web3auth, we sign in to our backend to get member profile
+  //   }
+  // }, [walletAddress])
 
 
   const sampleMetadata = {
@@ -312,6 +321,108 @@ export default function Home() {
     setMintNFTPlutusTxHash(txHash)
     setNftMintingPlutus(false)
   }
+
+  const mintNative = async () => {
+    setMintNativeLoading(true)
+    setMintNativeTxHash()
+
+    let _metadata = metadata ? JSON.parse(metadata) : sampleMetadata;
+    console.log("_metadata", _metadata);
+    
+    let asset = {
+      assetName: _metadata.name,
+      assetQuantity: '1',
+      metadata: _metadata,
+      label: '721',
+      recipient: cardanoWalletAddress
+    }
+
+    const nativeScript = {
+      type: "sig",
+      keyHash: cardanoPublicKeyHash
+    }
+    
+    const forgingScript = ForgeScript.fromNativeScript(nativeScript);
+
+    const _hash = resolveNativeScriptHash(nativeScript);
+    setNativeScriptPolicyId(_hash);
+
+    const tx = new Transaction({ initiator: cardanoWallet });
+
+    tx.mintAsset(
+      forgingScript,
+      asset,
+    );
+
+    const unsignedTx = await tx.build();
+    const signedTx = await cardanoWallet.signTx(unsignedTx);
+    const txHash = await cardanoWallet.submitTx(signedTx);
+    const response = await waitForTx(txHash)
+    console.log("response", response);
+    
+    setMintNativeTxHash(txHash)
+    setMintNativeLoading(false)
+  }
+
+  const burnNative = async () => {
+
+  }
+
+  // const mintWithHelios = async () => {
+  //   console.log("mintWithHelios");
+    
+  //   setMintNFTHeliosLoading(true)
+  //   setMintNFTHeliosTxHash()
+
+  //   let _metadata = metadata ? JSON.parse(metadata) : sampleMetadata;
+  //   console.log("_metadata", _metadata);
+
+  //   // Load the compiled Helios script (CBOR JSON)
+  //   // const plutusScript = {
+  //   //   type: "PlutusV2",
+  //   //   // Replace with actual compiled CBOR from Helios 
+  //   //   script: "59037159036e010000323232323232225333573464646464664a666ae68cdc3800a400024a666ae694ccd5cd19191980080099807002801112999aab9f00114a2264a666ae68c8ccd5cd000a504a2664464660020026602600600444a666aae7c0045288992999ab9a3233357340029412899b87330170050013301700400114a02660060066ae88008d5d080099807803800998078020008a50133003003357440046ae84004c94ccd5cd19b874800000452f5bded8c02646466ae80cdd80011ba633574066ec0004dd4001a5eb7bdb1812f5bded8c06ae84d5d11aab9e375400c6ae84d55cf1baa005480084cc034dd61aba135573c6ea80188cdd79aba135573c6ea80053010dd8799fd8799f43abc123ff00ff0014a026601200c9111c4d06e1860e2945c4502875eef31dce24d88c87f75009871976e6770a0014a024a666ae68cdc4191919199800800912999aab9f0011615333573466ebcd55ce9aba10010041375a6aae78d5d08008998010011aba200100622253335573e0022c2a666ae68cdd79aab9d3574200200a2660040046eacd55cf1aba10011333003003002357440026ae84d5d11aab9e375400a6ae84d55cf1baa004480004cc02401922011c4d06e1860e2945c4502875eef31dce24d88c87f75009871976e6770a0014a06aae74dd50030031bab35742600e0066466e9520003357406ea4008cd5d01ba90014bd70244100375c646ae84d55cf1baa001357426ae88d55cf1baa002357426aae78dd50008a4c2c446600c6eb0d5d09aba2357446ae88d5d11801801119baf001375200446ae88d5d11aba2357446aae78dd5000911919800800801912999aab9f00114bd6f7b6300a999ab9a3375e6aae74d5d080080189bab35573c6ae840044cc008008d5d1000911919191998008009998010010018020019112999aab9f001100213357406ae84004ccc00c00c008d5d10009112999aab9f00114bd700992999ab9a3300800523375e00200426660080080066ae880084cd5d00009998020020019aba200235573a6ae84004c8cc00400400c894ccd55cf8008a5eb804cd5d01aab9d35742002660040046ae8800488c8ccc00400400c0088894ccd55cf8010a5015333573460026ae8400852889998018019aba2002001223233300100100300222253335573e004290000a999ab9a3375e6aae74d5d080100089bad35573c6ae840084ccc00c00cd5d10010009" 
+  //   // };
+  //   const plutusScript = {"type": "PlutusScriptV2", "description": "", "cborHex": "59037159036e010000323232323232225333573464646464664a666ae68cdc3800a400024a666ae694ccd5cd19191980080099807002801112999aab9f00114a2264a666ae68c8ccd5cd000a504a2664464660020026602600600444a666aae7c0045288992999ab9a3233357340029412899b87330170050013301700400114a02660060066ae88008d5d080099807803800998078020008a50133003003357440046ae84004c94ccd5cd19b874800000452f5bded8c02646466ae80cdd80011ba633574066ec0004dd4001a5eb7bdb1812f5bded8c06ae84d5d11aab9e375400c6ae84d55cf1baa005480084cc034dd61aba135573c6ea80188cdd79aba135573c6ea80053010dd8799fd8799f43abc123ff00ff0014a026601200c9111c4d06e1860e2945c4502875eef31dce24d88c87f75009871976e6770a0014a024a666ae68cdc4191919199800800912999aab9f0011615333573466ebcd55ce9aba10010041375a6aae78d5d08008998010011aba200100622253335573e0022c2a666ae68cdd79aab9d3574200200a2660040046eacd55cf1aba10011333003003002357440026ae84d5d11aab9e375400a6ae84d55cf1baa004480004cc02401922011c4d06e1860e2945c4502875eef31dce24d88c87f75009871976e6770a0014a06aae74dd50030031bab35742600e0066466e9520003357406ea4008cd5d01ba90014bd70244100375c646ae84d55cf1baa001357426ae88d55cf1baa002357426aae78dd50008a4c2c446600c6eb0d5d09aba2357446ae88d5d11801801119baf001375200446ae88d5d11aba2357446aae78dd5000911919800800801912999aab9f00114bd6f7b6300a999ab9a3375e6aae74d5d080080189bab35573c6ae840044cc008008d5d1000911919191998008009998010010018020019112999aab9f001100213357406ae84004ccc00c00c008d5d10009112999aab9f00114bd700992999ab9a3300800523375e00200426660080080066ae880084cd5d00009998020020019aba200235573a6ae84004c8cc00400400c894ccd55cf8008a5eb804cd5d01aab9d35742002660040046ae8800488c8ccc00400400c0088894ccd55cf8010a5015333573460026ae8400852889998018019aba2002001223233300100100300222253335573e004290000a999ab9a3375e6aae74d5d080100089bad35573c6ae840084ccc00c00cd5d10010009"}
+
+  //   const tx = new Transaction({ initiator: cardanoWallet });
+  //   console.log(11);
+    
+  //   tx.mintAsset(
+  //     plutusScript,
+  //     {
+  //       // assetName: _metadata.name,  // Match TN = "" in Helios script
+  //       // amount: "1",    // Match QTY = 1 in Helios script
+  //       // redeemer: mesh.Data.to("Mint"), // Match Redeemer::Mint
+
+  //       assetName: _metadata.name,
+  //       assetQuantity: '1',
+  //       metadata: _metadata,
+  //       label: '721',
+  //       recipient: cardanoWalletAddress
+        
+  //     },
+  //     { data: mesh.Data.to("Mint") } // Pass the redeemer as a separate third argument
+  //   );
+  //   //.sendValue(cardanoWalletAddress, { lovelace: "2000000" })
+  //   console.log(22);
+  //   const unsignedTx = await tx.build();
+  //   const signedTx = await cardanoWallet.signTx(unsignedTx);
+  //   const txHash = await cardanoWallet.submitTx(signedTx);
+  //   const response = await waitForTx(txHash)
+  //   console.log("response", response);
+    
+  //   setMintNFTHeliosTxHash(txHash)
+  //   setMintNFTHeliosLoading(false)
+  // }
+
+  // const burnWithHelios = async () => {
+    
+  // }
+
+  // const transferWithHelios = async () => {
+    
+  // }
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -418,7 +529,7 @@ export default function Home() {
                   </div>
                 }
               </div>
-              <div className="underline mt-10 ">
+              {/* <div className="underline mt-10 ">
                 Mint NFT with Plutus script using MeshJs NFT Plutus contract (Aiken smart contract). Your wallet must have sufficient ADA as collateral.
               </div>
               <div className="flex flex-col ml-2">
@@ -451,7 +562,7 @@ export default function Home() {
                 }
               </div>
               <div className="flex flex-col ml-2 mt-10">
-                <h1>Mint NFT</h1>
+                <h1>Mint Asset</h1>
                 <input type="text" className="input" name="txtNFTCollectionName" placeholder="NFT Collection Name" value={oracleCollectionName} onChange={(e) => setOracleCollectionName(e.target.value)}/>
                 <input type="text" className="input mt-1" name="txtNFTParamUtxo" value={oracleMintParamUtxo} placeholder="NFT Param Utxo" onChange={(e) => setOracleMintParamUtxo(e.target.value)}/>
                 <button className="btn btn-primary mt-1" 
@@ -475,10 +586,37 @@ export default function Home() {
                   </div>
                   </>
                 }
-              </div>
+              </div> */}
               
-              <div>
-                
+              <div className="border p-4">
+                <span>Mint owneable asset with NativeScript</span>
+                <div className="border p-2">
+                  <button className="btn btn-primary" onClick={async() => await mintNative()}>
+                    {
+                      mintNativeLoading
+                      ?   <span className="flex justify-center">Loading.. <FontAwesomeIcon icon={faSpinner} className="h-4 w-4 animate-spin"/></span>
+                      :   <span>Mint with Native Script</span> 
+                    }
+                  </button>
+                  {
+                    nativeScriptPolicyId && 
+                    <div>
+                      <>Policy Id (this policy belong to your wallet) : </> {nativeScriptPolicyId}
+                    </div>
+                  }
+                  {
+                    mintNativeTxHash &&
+                    <>
+                    <div className="flex items-center">
+                      Transaction Hash: 
+                      <a target="_blank" href={`https://preview.cardanoscan.io/transaction/${mintNativeTxHash}`} className="ml-2 link">{mintNativeTxHash}</a>
+
+                      <span className="ml-2 text-xs">(Please wait 1 or 2 minute(s) before click on this link, cardanoscan will take some time)</span>
+                    </div>
+                    </>
+                  }
+                </div>
+
               </div>  
             </>
           : <button onClick={login} className="btn btn-primary">
